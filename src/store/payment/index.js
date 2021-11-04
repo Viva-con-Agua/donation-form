8be1120438e6e7d075e2a34313986e0bf2c69258
dennis <dennis_kleber@mailbox.org>
2021-11-04 20:26:26 +0100

    bug birthdate from string to int

--- src/store/payment/index.js
+++ src/store/payment/index.js
@@ -24,7 +24,7 @@ const payment = {
             first_name: "",
             last_name: "",
             street: "",
-            birthdate: "",
+            birthdate: 0,
             number: "",
             zip: "",
             city: "",
