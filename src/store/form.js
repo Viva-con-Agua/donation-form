b5165145989b4396be5caaed359a7f57cd5a7361
Tobi Ka <t.kaestle@vivaconagua.org>
2022-10-26 13:21:57 +0200

    added amount and interval to v2

--- src/store/form.js
+++ src/store/form.js
@@ -1,4 +1,4 @@
-import api from './api.js'
+import api from "./api.js";
 
 const form = {
     namespaced: true,
@@ -7,60 +7,67 @@ const form = {
             product: null,
             min_amount: 100,
             payment_types: [
-                { name: 'civisepa', title: 'payment.type.sepa', default: true },
-                { name: 'sepa', title: 'payment.type.sepa' },
-                { name: 'creditcard', title: 'payment.type.creditcard' },
-                { name: 'paypal', title: 'payment.type.paypal' }
+                { name: "civisepa", title: "payment.type.sepa", default: true },
+                { name: "sepa", title: "payment.type.sepa" },
+                { name: "creditcard", title: "payment.type.creditcard" },
+                { name: "paypal", title: "payment.type.paypal" },
             ],
             subscription_types: [
-                { name: 'civisepa', title: 'payment.type.sepa', default: true },
-                { name: 'sepa', title: 'payment.type.sepa' },
-                { name: 'creditcard', title: 'payment.type.creditcard' },
-                { name: 'paypal', title: 'payment.type.paypal' }
-            ]
-        }
+                { name: "civisepa", title: "payment.type.sepa", default: true },
+                { name: "sepa", title: "payment.type.sepa" },
+                { name: "creditcard", title: "payment.type.creditcard" },
+                { name: "paypal", title: "payment.type.paypal" },
+            ],
+        },
     }),
     getters: {
         product(state) {
-            return state.current.product
+            return state.current.product;
         },
         minAmount(state) {
-            return state.current.min_amount
+            return state.current.min_amount;
         },
         slider(state) {
-            return state.current.slider
+            return state.current.slider;
         },
         paymentTypes(state) {
-            return state.current.payment_types
+            return state.current.payment_types;
         },
         subscriptionTypes(state) {
-            return state.current.subscription_types
+            return state.current.subscription_types;
         },
         company(state) {
-            return state.current.company
-        }
+            return state.current.company;
+        },
     },
     mutations: {
         get(state, value) {
-            state.current = value
-        }
+            state.current = value;
+        },
     },
     actions: {
-        get({commit}, data) {
+        get({ commit }, data) {
             return new Promise((resolve, reject) => {
-                api.call.get('/v1/donations/form/' + data.data)
+                api.call
+                    .get("/v1/donations/form/" + data.data)
                     .then((response) => {
-                        commit('get', response.data.payload), 
-                        commit('company/current', response.data.payload),
-                        commit('payment/default_amount', response.data.payload.default_amount, {root: true}), 
-                        resolve()
+                        commit("get", response.data.payload),
+                            commit("company/current", response.data.payload, {
+                                root: true,
+                            }),
+                            commit(
+                                "payment/default_amount",
+                                response.data.payload.default_amount,
+                                { root: true }
+                            ),
+                            resolve();
                     })
                     .catch((error) => {
-                        reject(error)
-                    })
-            })
-        }
+                        reject(error);
+                    });
+            });
+        },
     },
-}
+};
 
-export default form
+export default form;
